5afbe0410fde742fcd035232725028110dcc9dc7
Mohamedamr9 <106562457+Mohamedamr9@users.noreply.github.com>
2024-06-23 15:40:00 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,27 @@
+$('#addPhotosBtn').click(function() {
+  $(this).parents().find('#addPhotosInput').click();
+});
+
+document.getElementById('addPhotosInput').onchange = e => {
+  const file = e.target.files[0];
+  const url = URL.createObjectURL(file);
+  const li = ` <li> <img src=" ${url} ">
+   <span><i class="fa fa-trash"></i></span>
+   </li>`
+  $('.photos-list ul').append(li);
+};
+
+$('#addVideosBtn').click(function() {
+  $(this).parents().find('#addVideosInput').click();
+});
+
+document.getElementById('addVideosInput').onchange = e => {
+  const file = e.target.files[0];
+  const url = URL.createObjectURL(file);
+  const li = ` <li> <video controls="controls" src=" ${url} " type="video/mp4" width="400px" height="200px"></video>
+       <span><i class="fa fa-trash"></i></span>
+   </li>`
+  $('.video-list ul').append(li);
+};
+
+document.getElementById('prediction').innerHTML = prediction;
